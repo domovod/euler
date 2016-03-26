c59cc193402f806ae5e0eb1515543c5cbaffd66f
Vladimir Tarasov <tarasov.vladimir@gmail.com>
2016-03-26 23:05:05 +0700

    Solving problem 5 (python)

---
+++ 005/problem.py
@@ -0,0 +1,43 @@
+# 005/problem.py
+
+# Problem URL: https://projecteuler.net/problem=5
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+# Comment: Too slow solution
+
+
+def divisible_by(a, b):
+    if b != 0:
+        if a % b == 0:
+            return True
+    return False
+
+
+def divider(a, b):
+    if b == 1:
+        return True
+    if divisible_by(a, b):
+        return divider(a, b-1)
+    return False
+
+
+def fact(n, acc):
+    if n == 0:
+        return acc
+    return fact(n-1, n*acc)
+
+
+def factorial(n):
+    return fact(n, 1)
+
+
+if __name__ == '__main__':
+    max_divider = 20
+    divisible = range(max_divider, factorial(max_divider)+1)
+    for i in divisible:
+        if divider(i, max_divider):
+            print('Answer:', i)
+            break

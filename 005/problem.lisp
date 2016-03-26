ef4d9acd4af2580dac5ed2d1438f3a70261094cc
Vladimir Tarasov <tarasov.vladimir@gmail.com>
2016-03-27 00:02:37 +0700

    Solving problem 5 (common lisp)

---
+++ 005/problem.lisp
@@ -0,0 +1,37 @@
+;;; 005/problem.lisp
+
+;;; Problem URL: https://projecteuler.net/problem=5
+;;; 2520 is the smallest number that can be divided by each of the numbers from 1
+;;; to 10 without any remainder.
+;;; What is the smallest positive number that is evenly divisible by all of the
+;;; numbers from 1 to 20?
+
+(defun fact (n acc)
+  (if (eql 0 n)
+      acc
+      (fact (- n 1) (* n acc))))
+
+(defun factorial (n)
+  (fact n 1))
+
+(defun divisiblep (a b)
+  (if (eql 0 (mod a b))
+      T
+      nil))
+
+(defun dividerp(a b)
+  (if (eql b 1)
+      T
+      (if (divisiblep a b)
+          (dividerp a (- b 1))
+          nil)))
+
+(defun find-div (acc val n)
+  (if (<= val acc)
+      (if (dividerp val n)
+          val
+          (find-div acc (+ val 1) n))
+      nil))
+
+(defun find-divisible (n)
+  (find-div (factorial n) n n))
